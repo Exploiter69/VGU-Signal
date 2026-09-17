f9bf6b7659031ead4a9d6f3906c883ff6f4825c9
Alok kumar <98121054+Exploiter69@users.noreply.github.com>
2026-09-17 12:34:47 +0530

    feat: expose phase 2 extraction API

--- src/vgu_signal/extraction/__init__.py
+++ src/vgu_signal/extraction/__init__.py
@@ -1,3 +1,35 @@
-from .html import PARSER_VERSION, extract_document
+from .common import classify_notice, extract_dates, extract_deadlines, extract_events, normalize_text, source_relative_id
+from .models import (
+    ExtractionKind,
+    ExtractionQuality,
+    ExtractedDate,
+    ExtractedDeadline,
+    ExtractedDocument,
+    ExtractedEvent,
+    NoticeCategory,
+    QualityLevel,
+)
+from .pdf import extract_pdf
+from .pipeline import extract_evidence
 
-__all__ = ["PARSER_VERSION", "extract_document"]
+__all__ = [
+    "ExtractionKind",
+    "ExtractionQuality",
+    "ExtractedDate",
+    "ExtractedDeadline",
+    "ExtractedDocument",
+    "ExtractedEvent",
+    "NoticeCategory",
+    "QualityLevel",
+    "classify_notice",
+    "extract_dates",
+    "extract_deadlines",
+    "extract_document",
+    "extract_evidence",
+    "extract_events",
+    "extract_pdf",
+    "normalize_text",
+    "source_relative_id",
+]
+
+from .html import extract_document
